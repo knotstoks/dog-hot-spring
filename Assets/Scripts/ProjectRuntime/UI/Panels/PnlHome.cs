1fc5697448b28eea66c89493429d458ed976c7a3
knotstoks <70320812+knotstoks@users.noreply.github.com>
2025-12-10 12:45:23 +0800

    Update ScHome to new UI

--- Assets/Scripts/ProjectRuntime/UI/Panels/PnlHome.cs
+++ Assets/Scripts/ProjectRuntime/UI/Panels/PnlHome.cs
@@ -16,6 +16,9 @@ namespace ProjectRuntime.UI.Panels
         public static PnlHome Instance { get; private set; }
 
         [field: SerializeField, Header("Scene References")]
+        private Animator AreaAnimator { get; set; }
+
+        [field: SerializeField]
         private Button SettingsButton { get; set; }
 
         [field: SerializeField]
@@ -25,15 +28,22 @@ namespace ProjectRuntime.UI.Panels
         private List<Button> LevelSelectButtons { get; set; }
 
         [field: SerializeField]
+        private List<TextMeshProUGUI> LevelSelectTMPs { get; set; }
+
+        [field: SerializeField]
         private Button PreviousAreaButton { get; set; }
 
         [field: SerializeField]
         private Button NextAreaButton { get; set; }
 
         [field: SerializeField, Header("Button Sprites")]
+        private Sprite RedButtonSprite { get; set; }
+
+        [field: SerializeField]
         private Sprite GrayButtonSprite { get; set; }
 
         private bool _isTransitioningScene;
+        private int _currentAreaIdx;
 
         private void Awake()
         {
@@ -47,6 +57,11 @@ namespace ProjectRuntime.UI.Panels
             }
 
             this.SettingsButton.OnClick(this.OnSettingsButtonClick);
+            for (var i = 0; i < this.LevelSelectButtons.Count; i++)
+            {
+                var temp = i + 1; // Neccessary to create temp variable for the closure function
+                this.LevelSelectButtons[i].OnClick(() => this.OnLevelSelectButtonClick(temp));
+            }
         }
 
         private void Start()
@@ -62,19 +77,48 @@ namespace ProjectRuntime.UI.Panels
         private async void Init()
         {
             this.ToggleAllButtonsShow(false);
+            var currentLevel = UserSaveDataManager.Instance.GetCurrentWorldProgress();
+            this._currentAreaIdx = currentLevel % 10 == 0 // 0-indexed
+                ? Mathf.Max(currentLevel / 10 - 1, 0)
+                : currentLevel / 10;
 
             await PanelManager.Instance.FadeFromBlack();
             if (!this) return;
 
+            this.RefreshUI(this._currentAreaIdx).Forget();
+        }
 
+        /// <summary>
+        /// This function moves the dolly over to whatever area page the ui is
+        /// </summary>
+        private async UniTask RefreshUI(int areaPageIdx)
+        {
+            this.ToggleAllButtonsShow(false);
+            this._currentAreaIdx = areaPageIdx;
+
+            // Change the level TMPs on the buttons
+            var firstLevelNumber = this._currentAreaIdx * 10 + 1;
+            for (var i = 0; i < this.LevelSelectTMPs.Count; i++)
+            {
+                this.LevelSelectTMPs[i].text = (firstLevelNumber + i).ToString();
+            }
+
+            // TODO: Move the camera using dolly?
+
+            this.ToggleAllButtonsShow(true);
         }
 
         public void ToggleAllButtonsShow(bool toggle)
         {
             this.SettingsButton.gameObject.SetActive(toggle);
             this.LevelSelectButtonParent.SetActive(toggle);
-            this.PreviousAreaButton.gameObject.SetActive(toggle);
-            this.NextAreaButton.gameObject.SetActive(toggle);
+
+            var numberOfWorlds = DWorld.GetAllData().Data.Count;
+            var numberOfPages = numberOfWorlds % 10 == 0
+                ? numberOfWorlds / 10
+                : numberOfWorlds / 10 + 1;
+            this.PreviousAreaButton.gameObject.SetActive(toggle && this._currentAreaIdx != 0);
+            this.NextAreaButton.gameObject.SetActive(toggle && this._currentAreaIdx < numberOfPages - 1);
         }
 
         #region Button Click
@@ -103,8 +147,8 @@ namespace ProjectRuntime.UI.Panels
             this._isTransitioningScene = true;
             this.ToggleAllButtonsShow(false);
 
-            // TODO
-
+            await this.RefreshUI(this._currentAreaIdx);
+            if (!this) return;
 
             this._isTransitioningScene = false;
             this.ToggleAllButtonsShow(true);
@@ -119,8 +163,8 @@ namespace ProjectRuntime.UI.Panels
             this._isTransitioningScene = true;
             this.ToggleAllButtonsShow(false);
 
-            // TODO
-
+            await this.RefreshUI(this._currentAreaIdx);
+            if (!this) return;
 
             this._isTransitioningScene = false;
             this.ToggleAllButtonsShow(true);
